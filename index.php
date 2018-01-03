c1a73c5f8f5b828aa9eafebd841fb69a32aa1343
Ruxton <ruxton@rhythmandpoetry.net>
2018-01-03 13:49:29 +0800

    Initial commit for indieweb-bestnine

---
+++ index.php
@@ -0,0 +1,10 @@
+<nav>
+  <h2>Indieweb BestNine</a></h2>
+</nav>
+
+<h1 class="title">Get your 2017 best nine photos<br>on your Indieweb site!</h1>
+
+<form action="/process.php" method="POST" class="input-group input-instagram">
+	<input name="url" type="text" class="form-control" placeholder="Indieweb URL" autocorrect="off" autocapitalize="off">
+	<input type="submit" class="getyourbest" value="GET">
+</form>
