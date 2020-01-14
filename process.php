3cf59eba93a87f43387f209f5893913d102ac82e
Ruxton <ruxton@rhythmandpoetry.net>
2020-01-14 11:31:34 +0800

    Log background processing output

--- process.php
+++ process.php
@@ -18,24 +18,23 @@ if(!$headerCheck || $headerCheck[0] == 'HTTP/1.1 404 Not Found') {
     $siteExists = false;
 }
 else {
-    $siteExists = true;
+  $siteExists = true;
+  if(!isset($siteUrl) || !$siteExists ) {
+  	header("Location: http://$host$path/nourl.php");
+  } else {
+  	header("Location: http://$host$path/result.php?key=".$siteKey);
+  	header('Connection: close');
+  	flush();
+  	if (session_id()) session_write_close();
+    $logfile = __DIR__.DIRECTORY_SEPARATOR."logs".DIRECTORY_SEPARATOR."$siteKey.txt";
+    file_put_contents($logfile, $siteUrl."\n", FILE_APPEND);
+  	$file = __DIR__.DIRECTORY_SEPARATOR."images".DIRECTORY_SEPARATOR."$siteKey.jpg";
+  	if(file_exists($file)) {
+  		exit;
+  	} else {
+  		$exec = "/usr/bin/php ".__DIR__.DIRECTORY_SEPARATOR."background_image_builder.php $siteUrl $year $month >> $logfile 2>>$logfile &";
+  		exec($exec);
+  	}
+  }
 }
-
-if(!isset($siteUrl) || !$siteExists ) {
-	header("Location: http://$host$path/nourl.php");
-} else {
-	header("Location: http://$host$path/result.php?key=".$siteKey);
-	header('Connection: close');
-	flush();
-	if (session_id()) session_write_close();
-
-	$file = __DIR__.DIRECTORY_SEPARATOR."images".DIRECTORY_SEPARATOR."$siteKey.jpg";
-	if(file_exists($file)) {
-		exit;
-	} else {
-		$exec = "/usr/bin/php ".__DIR__.DIRECTORY_SEPARATOR."background_image_builder.php $siteUrl $year $month > /dev/null 2>/dev/null &";
-		exec($exec);
-	}
-}
-
 ?>
