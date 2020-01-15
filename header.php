58b968c683b6ec5f64a9f25d988a1e8a45d14fb5
Ruxton <ruxton@rhythmandpoetry.net>
2020-01-15 14:34:38 +0800

    Adjust sub header text

--- header.php
+++ header.php
@@ -1,4 +1,4 @@
 <h2 class="ui header center aligned icon">
   <img class="ui icon" style="max-width: 80%;" src="/logo.png" alt="IndieWeb Best Nine" />
-  <div class="sub header">Get the best nine photos on your Indieweb site!</div>
+  <div class="sub header">Get the best nine photos from your Indieweb site!</div>
 </h2>
